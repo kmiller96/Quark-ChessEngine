e89fc4a9b6fa5b297abfa93e79a02c0528043edc
Kale Miller <kale.miller96@gmail.com>
2017-01-14 19:58:02 +0800

    [REFACTOR] Cleaned up vectors.py
    Small changes to the vector class, which was already pretty good. Just
    some minor changes here or there.

--- lib/vectors.py
+++ lib/vectors.py
@@ -12,66 +12,65 @@ class Vector:
     """Creates a 2D vector for the chess engine.
 
     This class contains all of the necessary backbone to do vector calculations.
-    It has the ability to add/subtract vectors, scalar multiply and do both the
-    dot product and the cross product. All of these abilities have native use
-    with the various operators.
+    These vectors are specially talored to fit in with the chess engine, making
+    the coordinates used only ever integers.
 
-    Public Methods
-    ================
-    The public methods are using the various operation symbols.
+    INITALISATION PARAMETERS
+    =========================
+    :rank: The rank of the position (0 <= x <= 7)
+    :File: The file of the position (0 <= x <= 7)
 
-    Private Methods
-    ================
-    :isvector: Returns true or false depending if the object passed is an
-        instance of the Vector class.
-    :eq: Determines if two vectors are equal.
-    :ne: Determines if the two vectors are unequal.
-    :add: Adds vectors together. Use the +/- characters.
-    :scalar_multiply: Scale a vector. Use the * character.
-    :dot: Dots vectors together. Use the * character.
-
-    Notes
-    ================
-    Currently no notes.
+    PUBLIC METHODS
+    ========================
+    :+: Add vectors together.
+    :-: Subtract vectors from one another.
+    :*: Either scalar multiply or dot product vectors.
+    :==: Equate vector components.
+    :!=: Unequate vector components.
+    :abs: Get the magnitude of the vector
+    :parallelto: Boolean return on if a vector is parallel to self.
+    :unitvector: Returns the quasi-unit vector of self.
     """
-    # TODO: Fix class to "Beg for Forgiveness" mentality.
 
-    def __init__(self, x, y):
+    def __init__(self, rank, File):
         """Initialise the Vector class."""
-        self.vector = (x, y)
+        self.vector = (rank, File)
 
     def __str__(self):
         """String representation of vector."""
         return str(self.vector)
 
-    def tupleform(self):
-        """A more obvious way of getting the vector as a tuple."""
-        return self.vector
-
-    def intmultipleof(self, other):
-        """See if self is an integer multiple of other."""
-        # FIXME: This is a clusterfuck.
+    def parallelto(self, other):
+        """See if self is parallel to other."""
         try:
-            intdivide = map(lambda x, y: x / y, self.vector, other.vector)
-            projected_selfvector = map(lambda i: intdivide[0]*i, other.vector)
-            if intdivide[0] != intdivide[1]:
-                return False
-            elif tuple(projected_selfvector) != self.vector:
-                return False
-            else:
+            # First exit if they are the same vector.
+            if self.vector == other.vector:
                 return True
+            # Now find which vector is larger.
+            elif self._mag() > other._mag():
+                longestvector = self.vector
+                shortestvector = other.vector
+            elif self._mag() < other._mag():
+                longestvector = other.vector
+                shortestvector = self.vector
+
+            # Now project the shorter vector onto the larger one.
+            integers = map(lambda x, y: x / y, longestvector, shortestvector)
+            projectedvector = map(lambda x, y: x*y, integers, shortestvector)
+
+            if tuple(projectedvector) == longestvector: return True
+            else: return False
         except AttributeError:
             raise AttributeError("Other must be a vector.")
 
     def unitvector(self):
         """Fetches a quasi-unit vector of the current vector."""
-        # REVIEW: Can this be made neater?
         if (self.vector[0] == 0 or self.vector[1] == 0):
             return Vector(*tuple(map(lambda i: int(i/self._mag()), self.vector)))
         elif abs(self.vector[0]) == abs(self.vector[1]):
             return Vector(*tuple(map(lambda i: int(i/abs(i)), self.vector)))
         else:
-            raise NotImplementedError("This call doesn't work for this vector.")
+            raise NotImplementedError
 
     def _scalar_multiply(self, intscalar):
         """Core for the scalar multiplication."""
@@ -95,11 +94,11 @@ class Vector:
         """Core for the multiplication."""
         try:
             if isinstance(other, int):
-                return Vector(*self._scalar_multiply(other))
+                return self._scalar_multiply(other)
             else:
                 return self._dot(other)
         except AttributeError:
-            raise AttributeError("Other must be a vector.")
+            raise AttributeError("Other must be a vector or scalar.")
 
     def __eq__(self, other):
         """Implement equality operations."""
@@ -161,12 +160,18 @@ class Vector:
 
     def __mul__(self, other):
         """Allows for dot product and scalar multiplication."""
-        return self._multiply(other)
+        try:
+            return Vector(*self._multiply(other))
+        except AttributeError:
+            raise AttributeError("Other must be a vector.")
 
     def __rmul__(self, other):
         """Reversed __mul__ method."""
-        return self._multiply(other)
+        try:
+            return Vector(*self._multiply(other))
+        except AttributeError:
+            raise AttributeError("Other must be a vector.")
 
     def __abs__(self):
         """Magnitude of the vector"""
-        return self._mag()
+        return int(self._mag())  # Only return integer lengths.
