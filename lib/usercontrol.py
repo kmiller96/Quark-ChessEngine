d6695672400d56e3a25d5669be3c8bdb0a0249af
Kale Miller <kale.miller96@gmail.com>
2017-01-14 20:42:51 +0800

    [REFACTOR] Completed cleaning UI class
    Moved all of the methods into the new usercontrol.py script. Made some
    small changes to the methods with renaming them and fixing the way they
    work while keeping their main functionality.

--- lib/usercontrol.py
+++ lib/usercontrol.py
@@ -13,6 +13,8 @@
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MAIN~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+from lib import core, vectors, pieces
+
  #     # ###
  #     #  #
  #     #  #
@@ -26,6 +28,87 @@ class EngineUI:
     """The class that handles the UI for the engine."""
 
     def __init__(self):
+        self.history = list()
+        self._symboltopiece = {
+            'R': pieces.RookPiece,
+            'N': pieces.KnightPiece,
+            'B': pieces.BishopPiece,
+            'Q': pieces.QueenPiece,
+            'K': pieces.KingPiece,
+            'P': pieces.PawnPiece
+        }
+        self._ranksymbols = '12345678'
+        self._filesymbols = 'abcdefgh'
+
+    def processusermove(self, userstring):
+        """Converts the movement notation into an action within the engine."""
+        # Determine what piece the user is using.
+        def determinepiece(symbol):
+            try:
+                return self._symboltopiece[symbol]
+            except KeyError:
+                if symbol.upper() in self._symboltopiece:
+                    raise NameError(
+                        "I don't know the piece %r. Did you mean %r?" % \
+                        (symbol, symbol.upper()))
+                else:
+                    raise core.UnknownPieceError(
+                        "I don't know the piece with the symbol %r." % symbol)
+
+        # Convert the notation into a positon.
+        def notationtopositions(notationstring):
+            try:
+                assert 'x' in notationstring or '->' in notationstring
+                (startpos, endpos) = (notationstring[:2], notationstring[-2:])
+
+                filefunc = lambda x: self._filesymbols.index(x[0])
+                rankfunc = lambda x: self._ranksymbols.index(x[1])
+
+                startvec = Vector(rankfunc(startpos), filefunc(startpos))
+                endvec = Vector(rankfunc(endpos), filefunc(endpos))
+            except AssertionError:
+                raise Exception(
+                    "The notation string doesn't follow correct syntax rules.")
+            else:
+                return startvec, endvec
+
+        # Now execute the process in order.
+        piecetomove = self.determinepiece(userstring[0])
+        startvec, endvec = self.notationtopositions(userstring[1:])
+        return piecetomove, (startvec, endvec)
+
+    def addmovetohistory(self, piecesymbol, startpos, endpos,
+                         capture=False, check=False, checkmate=False,
+                         castlelong=False, castleshort=False, promotionto=False):
+        """Add a move to the recorded history."""
+        # First turn the position into a notation string.
+        def convertpositiontonotation():
+            def getpositionstring(pos):
+                (rank_, file_) = core.convert(pos, tocoordinate=True)
+                return self._filesymbols[file_] + self._ranksymbols[rank_]
+
+            startnotation = getpositionstring(startpos)
+            endnotation = getpositionstring(endpos)
+            if capture: concat = 'x'
+            else: concat = '->'
+
+            movestring = startnotation + concat + endnotation
+
+            if check:
+                movestring += '+'
+            elif checkmate:
+                movestring += '#'
+            if castlelong:
+                movestring = '0-0-0'
+            elif castleshort:
+                movestring = '0-0'
+            if promotionto:
+                movestring += '=' + promotionto
+
+            return movestring
+
+        # Now add it to the history.
+        self.history.append(self.convertpositiontonotation())
         return None
 
   #####  #     # ###
