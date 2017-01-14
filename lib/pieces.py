9511490d4dd565d3a03a765705da71d16b5a7837
Kale Miller <kale.miller96@gmail.com>
2017-01-14 17:37:06 +0800

    [REFACTOR] Fully refactored piece classes to workable state
    The pieces, after being moved, have now been fully refactored into a
    clean state. By this, I mean that the pieces have lost their "bloated"
    methods, unused attributes and had a general facelift.
    
    As a part in the major refactoring, the chess pieces now are to be
    quite basic and dumb; they are now treated similar to the way they are
    on a physical board. As such, they have no concept of internal position
    on the board. They only know what their allowed directions of motion
    are.
    
    Lots of methods have been tagged for review instead of being deleted.
    This is because the internal workings of the code are of some use once
    the refactoring progresses (i.e. they will go elsewhere) but they don't
    below in the pieces class anymore.

--- lib/pieces.py
+++ lib/pieces.py
@@ -5,97 +5,64 @@
 # 7369626c65207461736b732e
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MAIN~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from lib import core
 
 class BasePiece:
     """The class all chess pieces inherit from."""
 
-    def __init__(self, colour, startpositionindex, notationsymbol,
-                 validmovevectors, onlyunitvectors=False):
+    def __init__(self, colour, notationsymbol, moveunitvectors, crawler=False):
         # Sanity checks.
-        assert isinstance(onlyunitvectors, bool), \
-            "'onlyunitvectors' parameter must be true or false."
+        assert isinstance(crawler, bool), \
+            "'crawler' parameter must be true or false."
         assert colour in ('white', 'black'), \
             "The colour of the piece must be 'white' or 'black'"
 
         # Assignment of attributes.
-        self._positionvector = self._tovector(startpositionindex)
-        self._notationsymbol = notationsymbol
-        self._validmovevectors = self._checkAllAreVectors(validmovevectors)
-        self._onlyunitvectors = onlyunitvectors
         self.colour = colour
+
+        if colour == 'white':
+            notationsymbol = notationsymbol.upper()
+        else:
+            notationsymbol = notationsymbol.lower()
+        self.notationsymbol = notationsymbol
+
+        for vec in moveunitvectors:
+            self._assertisvector(vec)
+        self._moveunitvectors = moveunitvectors
+        self._onlyunitvectors = crawler
         return None
 
     def __str__(self):
         """How to print the class if called as a string."""
         return self.__class__.__name__
 
-    def _sametype(self, other):
-        """Sees if the other is the same type of piece as this one."""
-        return other.__class__.__name__ == self.__class__.__name__
-
     def __eq__(self, other):
-        """Controls equality."""
-        return self._sametype(other)
+        """Controls equality. Checks to see if the same type."""
+        return other.__class__.__name__ == self.__class__.__name__
 
     def __ne__(self, other):
         """Controls inequality."""
-        return not self._sametype(other)
+        return other.__class__.__name__ != self.__class__.__name__
 
     @staticmethod
-    def _checkAllAreVectors(vectorlist):
+    def _assertisvector(vector):
         """A sanity check to make sure all items in vectorlist are vectors."""
         try:
-            for item in vectorlist:
-                assert isinstance(item, Vector)
-            return vectorlist
-        except TypeError as error1:
-            raise TypeError("Arg1 must be an iterable.")
-        except AssertionError as error2:
-            raise AssertionError("There are non-vectors in the list.")
+            assert isinstance(item, core.Vector)
+        except AssertionError:
+            raise AssertionError("The item %r isn't a vector." % item)
 
-    @staticmethod
-    def _tovector(indexorvector):
-        """Converts an index on the board into a vector."""
-        try:
-            if isinstance(indexorvector, Vector):
-                vector = indexorvector
-                return vector
-            else:
-                index = indexorvector
-                assert isinstance(index, int)
-                assert 0 <= index <= 63
-        except AssertionError as error:
-            raise TypeError("The argument must be an index from 0 to 63.")
-        else:
-            return Vector(index/8, index % 8)
-
-    @staticmethod
-    def _toindex(indexorvector):
-        """Converts a vector on the board to an index."""
-        try:
-            if isinstance(indexorvector, int):
-                index = indexorvector
-                return index
-            else:
-                vector = indexorvector
-                return vector.vector[0]*8 + vector.vector[1]
-        except AttributeError as error:
-            raise TypeError("The argument must be a vector.")
-
-    def piecetype(self):
+    def type(self):
         """Returns the class of which this piece inherits from."""
-        # NOTE: This has no catches in place for bad inputs!
         return eval(self.__class__.__name__)
 
-    def symbol(self, forasciiboard=False):
-        """Fetches the symbol of the piece. Optional argument is a WIP."""
-        return self._notationsymbol
-
     def distancefromselfto(self, moveto):
+        # TODO: Move this method into somewhere else, or remove it.
         """Find the relative vector between board index and current position."""
-        return self._tovector(moveto) - self._positionvector
+        return core.convert(moveto, tovector=True) - self._positionvector
 
     def possiblemoves(self):
+        # TODO: Move this method to the movegeneration file.
         """Gets possibles moves for piece if board was empty.
 
         The chess board controls whether the move is legal or not in terms of
@@ -117,27 +84,6 @@ class BasePiece:
                 if self._onlyunitvectors: break  # Don't loop if onlyunitvectors.
                 posvector += unitvector
         return possiblemoveslist
-
-    def position(self, indexform=False, vectorform=False):
-        """Returns the position of the piece. Used for encapsulation purposes."""
-        assert (indexform or vectorform), \
-            "Specify either index-form or vector-form of returned position."
-        assert not (indexform and vectorform), \
-            "Can only return either index-form or vector-form."
-
-        if indexform: return self._toindex(self._positionvector)
-        elif vectorform: return self._positionvector
-        else: raise RuntimeError("Something went wrong.")
-
-    def movetoindex(self, index):
-        """Moves the piece to new index."""
-        self._positionvector = self._tovector(index)
-        return None
-
-    def movetovector(self, vector):
-        """Moves the piece to the position specified by the vector."""
-        self._positionvector = vector
-        return None
 
 
 class RookPiece(BasePiece):
@@ -168,7 +114,7 @@ class BishopPiece(BasePiece):
     """The class for the bishop."""
 
     def __init__(self, colour, startpositionindex):
-        BasePiece.__init__(self, colour, startpositionindex, 'B',
+        BasePiece.__init__(self, colour, 'B',
             validmovevectors=(
                 Vector(1, 1), Vector(1, -1), Vector(-1, -1), Vector(-1, 1))
         )
@@ -178,7 +124,7 @@ class QueenPiece(BasePiece):
     """The class for the queen."""
 
     def __init__(self, colour, startpositionindex):
-        BasePiece.__init__(self, colour, startpositionindex, 'Q',
+        BasePiece.__init__(self, colour, 'Q',
             validmovevectors=(
                 Vector(1, 0), Vector(0, 1), Vector(1, 1), Vector(1, -1),
                 Vector(-1, 0), Vector(0, -1), Vector(-1, -1), Vector(-1, 1))
@@ -190,7 +136,7 @@ class KingPiece(BasePiece):
     """The class for the King"""
 
     def __init__(self, colour, startpositionindex):
-        BasePiece.__init__(self, colour, startpositionindex, 'K',
+        BasePiece.__init__(self, colour, 'K',
             validmovevectors=(
                 Vector(1, 0), Vector(0, 1), Vector(1, 1), Vector(1, -1),
                 Vector(-1, 0), Vector(0, -1), Vector(-1, -1), Vector(-1, 1)),
@@ -201,7 +147,6 @@ class KingPiece(BasePiece):
 
 class PawnPiece(BasePiece):
     """The very special class for the pawn."""
-    # WIP: Still be fixed.
 
     def __init__(self, colour, startpositionindex):
         if colour == 'white':
@@ -213,20 +158,14 @@ class PawnPiece(BasePiece):
             self._captureleft = Vector(-1, -1)
             self._captureright = Vector(-1, 1)
 
-        BasePiece.__init__(self, colour, startpositionindex, "",
+        BasePiece.__init__(self, colour, startpositionindex, "P",
             validmovevectors=(movevector,), onlyunitvectors=True
         )
-        self._validcapturemoves = (Vector(1, 1), Vector(1, -1))
         return None
-
-    def symbol(self, forasciiboard=False):
-        """A hack-fix of symbol in order to use the same method in the UI"""
-        if forasciiboard: return 'P'
-        else: return ""
 
     def isvalidcapture(self, movetopos):
         """Pawns capture in a strange fashion. This method controls that."""
-        # REVIEW: Can this be moved into chessboard class?
+        # REVIEW: Do I ever call this method?
         diffvec = self.distancefromselfto(movetopos)
         capturemoves = (self._captureleft, self._captureright)
         return (diffvec in self._validcapturemoves)
