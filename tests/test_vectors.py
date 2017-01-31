9e39ea6ad38844e3b6b6d4c456145a298be14d68
Kale Miller <kale.miller96@gmail.com>
2017-01-31 09:35:36 +0800

    [ADD] Unit tests on the Vector class for addition and subtraction
    There are now a couple tests for the addition and subtraction operators
    (+ and - respectively) to make sure that they are behaving correctly.
    These tests don't look for bad inputs but just that the output is as
    expected.

---
+++ tests/test_vectors.py
@@ -0,0 +1,112 @@
+# DESCRIPTION: Tests the vector class.
+
+# 4920646f6e5c2774206361726520696620697420776f726b73206f6e20796f7572206d61636869
+# 6e652120576520617265206e6f74207368697070696e6720796f7572206d616368696e6521
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import unittest
+from lib import chessvectors as vectors
+
+def errormessage(resultstring, expectedstring):
+    """Does the formatting for the error message"""
+    return "\n\nRESULT: %s.\nEXPECTED: %s." % (resultstring, expectedstring)
+
+
+class BasicVectorPlusOperator(unittest.TestCase):
+    """Tests the plus operator for the Vector class."""
+
+    def setUp(self):
+        self.vector1 = vectors.Vector(3, 3)
+        self.vector2 = vectors.Vector(5, 5)
+        self.ans = (8, 8)
+        return None
+
+    def test_privatemethod_add(self):
+        self.assertEqual(
+            tuple(self.vector1._add(self.vector2)), self.ans,
+            errormessage(
+                str(tuple(self.vector1._add(self.vector2))),
+                str(self.ans)
+            )
+        )
+        return None
+
+    def test_addvectors(self):
+        resultant1 = self.vector1 + self.vector2
+        resultant2 = self.vector2 + self.vector1
+
+        self.assertEqual(
+            resultant1.vector, self.ans,
+            errormessage(
+                "%s + %s = %s" % (self.vector1, self.vector2, resultant1),
+                "%s + %s = %s" % (self.vector1, self.vector2, self.ans)
+            )
+        )
+        self.assertEqual(
+            resultant2.vector, self.ans,
+            errormessage(
+                "%s + %s = %s" % (self.vector1, self.vector2, resultant2),
+                "%s + %s = %s" % (self.vector1, self.vector2, self.ans)
+            )
+        )
+        return None
+
+    def test_plusequaloperator(self):
+        resultant = self.vector1
+        resultant += self.vector2
+        ans = (8, 8)
+        self.assertEqual(
+            resultant.vector, self.ans,
+            errormessage(
+                "%s += %s -> %s" % (self.vector1, self.vector2, resultant),
+                "%s += %s -> %s" % (self.vector1, self.vector2, self.ans)
+            )
+        )
+        return None
+
+
+class BasicVectorMinusOperator(unittest.TestCase):
+    """Tests the overloaded minus operator."""
+
+    def setUp(self):
+        self.vector1 = vectors.Vector(3, 3)
+        self.vector2 = vectors.Vector(5, 5)
+        self.ans1 = (-2, -2)
+        self.ans2 = (2, 2)
+        return None
+
+    def test_subtractvectors(self):
+        resultant1 = self.vector1 - self.vector2
+        resultant2 = self.vector2 - self.vector1
+
+        self.assertEqual(
+            resultant1.vector, self.ans1,
+            errormessage(
+                "%s - %s = %s" % (self.vector1, self.vector2, resultant1),
+                "%s - %s = %s" % (self.vector1, self.vector2, self.ans1)
+            )
+        )
+        self.assertEqual(
+            resultant2.vector, self.ans2,
+            errormessage(
+                "%s - %s = %s" % (self.vector1, self.vector2, resultant2),
+                "%s - %s = %s" % (self.vector1, self.vector2, self.ans2)
+            )
+        )
+        return None
+
+    def test_minusequaloperator(self):
+        resultant = self.vector1
+        resultant -= self.vector2
+        self.assertEqual(
+            resultant.vector, self.ans1,
+            errormessage(
+                "%s -= %s -> %s" % (self.vector1, self.vector2, resultant),
+                "%s -= %s -> %s" % (self.vector1, self.vector2, self.ans1)
+            )
+        )
+        return None
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
