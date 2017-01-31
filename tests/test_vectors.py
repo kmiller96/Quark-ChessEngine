175d187a52a92f88c906dd85476f7cd1c18f8451
Kale Miller <kale.miller96@gmail.com>
2017-01-31 16:29:32 +0800

    [ADD] Tests for misc calls and bad inputs on vectors
    There are now tests in place for the miscellaneous calls like str() and
    the __init__ which would otherwise rarely get any attention. The other
    addition is that there are now tests confirming that incorrect inputs
    are handled appropriately.

--- tests/test_vectors.py
+++ tests/test_vectors.py
@@ -13,6 +13,79 @@ def errormessage(resultstring, expectedstring):
     return "\n\nRESULT: %s.\nEXPECTED: %s." % (resultstring, expectedstring)
 
 
+class MiscellaneousVectorCalls(unittest.TestCase):
+    """These tests are for the random methods that lie around in the class."""
+
+    def setUp(self):
+        self.vector = vectors.Vector(1, 2)
+
+    def test_init(self):
+        self.assertEqual(
+            self.vector.vector, (1, 2)
+        )
+        return None
+
+    def test_str(self):
+        self.assertEqual(
+            str(self.vector), "(1, 2)"
+        )
+        return None
+
+    def test_equality(self):
+        self.assertTrue(self.vector == vectors.Vector(1, 2))
+        self.assertFalse(self.vector == vectors.Vector(0, 5))
+        return None
+
+    def test_inequality(self):
+        self.assertTrue(self.vector != vectors.Vector(0, 9))
+        self.assertFalse(self.vector != vectors.Vector(1, 2))
+
+
+class BadInputVectorTests(unittest.TestCase):
+    """Use bad inputs on the methods and see if they fail appropriately."""
+
+    def setUp(self):
+        self.vector = vectors.Vector(2, 3)
+        self.stringinput = ("2", "3")
+        self.floatinput = (2.1, 3.0)
+        return None
+
+    def test_initparametersarestrings(self):
+        with self.assertRaises(TypeError):
+            vectors.Vector(*self.stringinput)
+        return None
+
+    def test_initparametersarefloats(self):
+        with self.assertRaises(TypeError):
+            vectors.Vector(*self.floatinput)
+        return None
+
+    def test_addnonvector(self):
+        with self.assertRaises(TypeError):
+            self.vector + (1, 5)
+        return None
+
+    def test_subtractnonvector(self):
+        with self.assertRaises(TypeError):
+            self.vector - "(-3, 5)"
+        return None
+
+    def test_multiplybadscalar(self):
+        with self.assertRaises(TypeError):
+            self.vector * 9.1
+        return None
+
+    def test_multiplybadvector(self):
+        with self.assertRaises(TypeError):
+            self.vector * (1, 5)
+        return None
+
+    def test_equalitynonvector(self):
+        with self.assertRaises(TypeError):
+            self.vector == (1, 2)
+        return None
+
+
 class BasicVectorPlusOperator(unittest.TestCase):
     """Tests the plus operator for the Vector class."""
 
