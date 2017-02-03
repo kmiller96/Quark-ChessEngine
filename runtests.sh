5ec166997dce68f69a18e36764b954a38bedac1f
Kale Miller <kale.miller96@gmail.com>
2017-02-03 10:20:52 +0800

    [NEW] A bash scipt that runs all of the tests
    At the end of a session, there is now the ability to run all of the
    tests that have been written by calling this bash script. It is simple
    but effective: it just runs the commandline call with some echos.

---
+++ runtests.sh
@@ -0,0 +1,38 @@
+echo ""
+echo "CORE TESTS"
+echo "==================================="
+python -m tests/test_core
+
+echo ""
+echo "PIECE TESTS"
+echo "==================================="
+python -m tests/test_pieces
+
+echo ""
+echo "VECTOR TESTS"
+echo "==================================="
+python -m tests/test_vectors
+
+echo ""
+echo "GUI & UI TESTS"
+echo "==================================="
+#python -m tests/test_usercontrol
+echo "There is nothing here yet..."
+
+echo ""
+echo "CHESSBOARD TESTS"
+echo "==================================="
+#python -m tests/test_chessboard
+echo "There is nothing here yet..."
+
+echo ""
+echo "MOVEGEN TESTS"
+echo "==================================="
+#python -m tests/test_movegenerator
+echo "There is nothing here yet..."
+
+echo ""
+echo "ENGINE TESTS"
+echo "==================================="
+#python -m tests/test_engine
+echo "There is nothing here yet..."
