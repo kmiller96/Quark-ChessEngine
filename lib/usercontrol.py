1af78b256d2c4b2143bfc62b6a465d2be33591c8
Kale Miller <kale.miller96@gmail.com>
2017-02-07 13:01:15 +0800

    Removed old, random comments

--- lib/usercontrol.py
+++ lib/usercontrol.py
@@ -4,13 +4,6 @@
 # 737369626c65207461736b20696e746f207365766572616c207665727920736d616c6c20706f73
 # 7369626c65207461736b732e
 
-# NOTE:
-# ================
-# This file is currently a work in progress. The original chessboard.py file has
-# become overly bloated and is detrimental to the moral of the project. This new
-# script is aimed at taking only the methods that pertain to the UI and GUI,
-# while separating the other components into their own scrips
-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~MAIN~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 from lib import core, vectors, pieces
