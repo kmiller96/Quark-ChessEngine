ed96c387904edfab4776975e6b433258ae9c9494
Kale Miller <kale.miller96@gmail.com>
2017-02-08 09:05:07 +0800

    [ADD] Testing suite for movegenerator.py
    The first tests added where for the _piecesonsameside method. There is
    now framework for testing the movegenerator script.

---
+++ tests/test_movegenerator.py
@@ -0,0 +1,47 @@
+# DESCRIPTION: Tests the move generator.
+
+# 4920646f6e5c2774206361726520696620697420776f726b73206f6e20796f7572206d61636869
+# 6e652120576520617265206e6f74207368697070696e6720796f7572206d616368696e6521
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import unittest
+from lib import core, chessboard, pieces, movegenerator
+from tests.test_core import errormessage
+
+class TestCoreMoveGenerator(unittest.TestCase):
+    """Goes about testing the core methods of the move generator."""
+
+    def setUp(self):
+        self.board = chessboard.ChessBoard()
+        self.board[18] = pieces.QueenPiece('white')
+        self.board[19] = pieces.PawnPiece('white')
+        self.board[23] = pieces.RookPiece('black')
+        self.board[42] = pieces.KnightPiece('black')
+
+        self.generator = movegenerator._CoreMoveGenerator(self.board)
+
+    def test_piecesareonsameside_white(self):
+        self.assertTrue(
+            self.generator._piecesareonsameside(self.board[18], self.board[19]),
+            errormessage(False, True)
+        )
+        return None
+
+    def test_piecesareonsameside_black(self):
+        self.assertTrue(
+            self.generator._piecesareonsameside(self.board[23], self.board[42]),
+            errormessage(False, True)
+        )
+        return None
+
+    def test_piecesareonsameside_bothcolours(self):
+        self.assertFalse(
+            self.generator._piecesareonsameside(self.board[18], self.board[23]),
+            errormessage(True, False)
+        )
+        return None
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
