d66c9b901493aa86953cf1340e77cd5b55b96ef4
Kale Miller <kale.miller96@gmail.com>
2017-02-14 13:46:24 +0800

    [ADD] Tests for the engine's evaluation function

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -4,3 +4,47 @@
 # 6e652120576520617265206e6f74207368697070696e6720796f7572206d616368696e6521
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+import unittest
+from lib import chessboard, core, engine, movegenerator, pieces, usercontrol
+
+class TestEvaluation(unittest.TestCase):
+    """Does basic tests on the evaluation algorithm, making sure that the values
+    it assigns are accurate based on the position."""
+
+    def setUp(self):
+        self.board = chessboard.ChessBoard()
+        self.board[60] = pieces.KingPiece('black')
+        self.board[4] = pieces.KingPiece('white')
+
+        self.eval = engine.EngineEvaluation
+        return None
+
+    def test_uppiece_white(self):
+        self.board[55] = pieces.QueenPiece('white')
+        evaluator = self.eval(self.board)
+        self.assertGreaterEqual(evaluator.evaluateposition(), 9)
+        return None
+
+    def test_uppiece_black(self):
+        self.board[55] = pieces.QueenPiece('black')
+        evaluator = self.eval(self.board)
+        self.assertLessEqual(evaluator.evaluateposition(), -9)
+        return None
+
+    def test_equalpawns_whitedoubled(self):
+        self.board[51] = pieces.PawnPiece('black')
+        self.board[52] = pieces.PawnPiece('black')
+        self.board[53] = pieces.PawnPiece('black')
+
+        self.board[11] = pieces.PawnPiece('white')
+        self.board[12] = pieces.PawnPiece('white')
+        self.board[20] = pieces.PawnPiece('white')
+
+        evaluator = self.eval(self.board)
+        print evaluator.evaluateposition()
+        self.assertLess(evaluator.evaluateposition(), 0)
+        return None
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
